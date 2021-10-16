469a35c36f1e9046f6d75779fc2f16cfc44d7868
Mihai Sorin Dobrescu <7279637+msdobrescu@users.noreply.github.com>
2021-10-16 20:44:03 +0300

    No push rights - remove --push

--- .github/build.sh
+++ .github/build.sh
@@ -3,5 +3,5 @@
 sudo -E luet build \
         --only-target-package \
         --pull-repository $PULL_REPOSITORY \
-        --pull --push --image-repository $FINAL_REPO \
+        --pull --image-repository $FINAL_REPO \
         --from-repositories --no-spinner --live-output --tree $PWD/packages "$1"
