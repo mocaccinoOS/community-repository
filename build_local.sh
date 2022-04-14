6bbbc512a8c61faa4343838fba408fc44bb2fc69
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-04-14 20:32:41 +0200

    Update build_local.sh

--- build_local.sh
+++ build_local.sh
@@ -6,5 +6,6 @@ docker build -t build-image $root
 docker run --rm -ti -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:/workdir --entrypoint /usr/bin/luet -w /workdir build-image build \
       -q --only-target-package \
       --pull-repository quay.io/mocaccino/desktop \
+      --pull-repository quay.io/mocaccino/os-commons \
       --pull --image-repository local-repo --from-repositories --no-spinner --live-output --tree packages "$@"
 
