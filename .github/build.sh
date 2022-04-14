4c02ddeacdd7570e747e70427937c3b33a4a1317
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-04-14 20:32:21 +0200

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -2,6 +2,7 @@
 
 sudo -E luet build \
         --only-target-package \
+        --pull-repository quay.io/mocaccino/os-commons \
         --pull-repository $PULL_REPOSITORY \
         --pull --push --image-repository $FINAL_REPO \
         --from-repositories --no-spinner --live-output --tree $PWD/packages "$1"
