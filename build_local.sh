36700969bb3fde7fa526f5d9266bccc70e64bd95
Sorin Mihai Dobrescu <msdobrescu@gmail.com>
2024-08-19 14:05:04 +0300

    build dev test
    ... and a typo ...

--- build_local.sh
+++ build_local.sh
@@ -12,7 +12,8 @@ pushd $root
 docker build -t build-image $root
 
 docker run --rm -ti -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:/workdir --entrypoint /usr/bin/luet -w /workdir build-image build \
-      -q --only-target-package \
+      --quiet \
+      --only-target-package \
       --pull-repository quay.io/mocaccino/desktop \
       --pull-repository quay.io/mocaccino/os-commons \
       --pull-repository quay.io/mocaccino/extra \
