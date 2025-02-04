6424d0a3f93012965db335622676028b38e0a463
Sorin Mihai Dobrescu <msdobrescu@gmail.com>
2025-02-04 22:12:07 +0200

    rollback, cleanup

--- diagrams/generate.sh
+++ diagrams/generate.sh
@@ -10,7 +10,7 @@
 ( \
 echo -e '@startuml\n'; \
 (yq r -j ../packages/apps/collection.yaml \
-| jq -r '{buildbase: "<< (B,#6495ED) >>", layerbase: "<< (λ,#98FB98) >>", layers: "<< (L,#9ACD32) >>", libs: "<< (l,#FF7700) >>", apps: "<< (a,#FF00FF) >>", utils: "<< (u,#4B0082) >>", fonts: "<< (f,#48D1CC) >>", development: "<< (d,#D4AC0D) >>", entity: "<< (e,#5DADE2) >>"} as $packages_styles
+| jq -r '{buildbase: "<< (β,#6495ED) >>", layerbase: "<< (λ,#98FB98) >>", layers: "<< (ℒ,#9ACD32) >>", libs: "<< (l,#FF7700) >>", apps: "<< (a,#FF00FF) >>", utils: "<< (u,#4B0082) >>", fonts: "<< (f,#48D1CC) >>", development: "<< (d,#D4AC0D) >>", entity: "<< (e,#5DADE2) >>"} as $packages_styles
        | {buildbase: "<color:#6495ED><U+2756></color>", layerbase: "<color:#98FB98><U+2756></color>", layers: "<color:#9ACD32><U+2756></color>", libs: "<color:#FF7700><U+1F79B></color>", apps: "<color:#FF00FF><U+25C9></color>", utils: "<color:#4B0082><U+2318></color>", fonts: "<color:#48D1CC><U+24D5></color>", development: "<color:#D4AC0D><U+24D3></color>", entity: "<color:#5DADE2><U+24D4></color>", system: "<color:#DC7633><U+24E2></color>", provides: "<color:#34495E><U+24DF></color>"} as $deps_styles
        | .packages[]
        | [(if (.optrequires) then .requires[] + .optrequires[] else .requires[] end)] as $deps
@@ -31,7 +31,7 @@ echo -e '\n@enduml'; \
 ( \
 echo -e '@startuml\n'; \
 (yq r -j ../packages/apps/collection.yaml \
-| jq -r '{buildbase: "<< (B,#6495ED) >>", layerbase: "<< (λ,#98FB98) >>", layers: "<< (L,#9ACD32) >>", libs: "<< (l,#FF7700) >>", apps: "<< (a,#FF00FF) >>", utils: "<< (u,#4B0082) >>", fonts: "<< (f,#48D1CC) >>", development: "<< (d,#D4AC0D) >>", entity: "<< (e,#5DADE2) >>"} as $packages_styles
+| jq -r '{buildbase: "<< (β,#6495ED) >>", layerbase: "<< (λ,#98FB98) >>", layers: "<< (ℒ,#9ACD32) >>", libs: "<< (l,#FF7700) >>", apps: "<< (a,#FF00FF) >>", utils: "<< (u,#4B0082) >>", fonts: "<< (f,#48D1CC) >>", development: "<< (d,#D4AC0D) >>", entity: "<< (e,#5DADE2) >>"} as $packages_styles
        | {buildbase: "<color:#6495ED><U+2756></color>", layerbase: "<color:#98FB98><U+2756></color>", layers: "<color:#9ACD32><U+2756></color>", libs: "<color:#FF7700><U+1F79B></color>", apps: "<color:#FF00FF><U+25C9></color>", utils: "<color:#4B0082><U+2318></color>", fonts: "<color:#48D1CC><U+24D5></color>", development: "<color:#D4AC0D><U+24D3></color>", entity: "<color:#5DADE2><U+24D4></color>", system: "<color:#DC7633><U+24E2></color>", provides: "<color:#34495E><U+24DF></color>"} as $deps_styles
        | .packages[]
        | [(if (.optrequires) then .requires[] + .optrequires[] else .requires[] end)] as $deps
@@ -53,7 +53,7 @@ echo -e '\n@enduml'; \
 ( \
 echo -e '@startuml\n'; \
 (yq r -j ../packages/apps/collection.yaml \
-| jq -r '{buildbase: "<< (B,#6495ED) >>", layerbase: "<< (λ,#98FB98) >>", layers: "<< (L,#9ACD32) >>", libs: "<< (l,#FF7700) >>", apps: "<< (a,#FF00FF) >>", utils: "<< (u,#4B0082) >>", fonts: "<< (f,#48D1CC) >>", development: "<< (d,#D4AC0D) >>", entity: "<< (e,#5DADE2) >>"} as $packages_styles
+| jq -r '{buildbase: "<< (β,#6495ED) >>", layerbase: "<< (λ,#98FB98) >>", layers: "<< (ℒ,#9ACD32) >>", libs: "<< (l,#FF7700) >>", apps: "<< (a,#FF00FF) >>", utils: "<< (u,#4B0082) >>", fonts: "<< (f,#48D1CC) >>", development: "<< (d,#D4AC0D) >>", entity: "<< (e,#5DADE2) >>"} as $packages_styles
        | .packages[]
        | "entity \"\(.name)\" as \(.category)/\(.name) \($packages_styles[.category]) {\n\n}"'); \
 echo -e '\n'; \
